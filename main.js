1a32d625f0744e15d773bb116291fa420a0c425f
Lukas Krivka <lukaskrivka@gmail.com>
2018-10-15 16:30:21 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,119 @@
+const Apify = require('apify')
+const {google} = require('googleapis');
+const csvParser =require('csvtojson')
+const {apifyGoogleAuth} = require('apify-google-auth')
+
+const {toRows, toObjects, append, countCells, trimSheetRequest} = require('./utils')
+
+const MAX_CELLS = 2 * 1000 * 1000
+
+Apify.main(async()=>{
+    const input = await Apify.getValue('INPUT')
+    console.log('input')
+    console.dir(input)
+
+    const authOptions = {
+        scope: "spreadsheets",
+        tokensStore: input.tokensStore
+    }
+    const auth = await apifyGoogleAuth(authOptions)
+
+    // loadData
+    const defaultOptions = {
+        format: 'csv',
+        limit: input.limit,
+        offset: input.offset,
+    }
+
+    let csv
+    
+    csv = await Apify.client.datasets.getItems({
+        datasetId: input.datasetOrExecutionId,
+        ...defaultOptions
+    }).then(res=>res.items.toString()).catch(console.log)
+    
+    if(!csv){
+        csv = await Apify.client.crawler.getExecutionResults({
+            executionId: input.datasetOrExecutionId,
+            ...defaultOptions
+        }).then(res=>res.items.toString()).catch(console.log)
+    }
+
+    console.log('Data loaded from Apify storage')
+
+    if(!csv) throw (`We didn't find any dataset or crawler execution with provided datasetOrExecutionId`)
+
+    const newObjects = await csvParser().fromString(csv)
+
+    const sheets = google.sheets({version: 'v4', auth});
+
+    const {spreadsheetId, mode, filterByField, filterByEquality} = input
+
+    const spreadsheetMetadata = await sheets.spreadsheets.get({spreadsheetId})
+    const {title: firstSheetName, sheetId: firstSheetId} = spreadsheetMetadata.data.sheets[0].properties
+    console.log('name of the first sheet', firstSheetName)
+    console.log('id of the first sheet', firstSheetId)
+
+    const range = input.range || firstSheetName
+    
+    console.log(`Spreadsheets setup:`)
+    console.log('mode:',mode)
+    console.log('spreadsheet id:',spreadsheetId)
+    console.log('range:',range)
+    console.log('filter by field:', filterByField)
+    console.log('filter by equality:', filterByEquality)
+
+    let rowsToInsert
+    if(mode === 'replace'){
+        rowsToInsert = toRows(newObjects)
+    }
+    if(mode === 'append'){
+        rowsResponse = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range
+        }).catch(e=>console.log('getting previous rows failed with error:',e.message))
+        const previousRows = rowsResponse.data.values
+        if(!previousRows) throw (`We couldn't get previous rows so we cannot compare!`)
+
+        const previousObjects = toObjects(previousRows)
+        const appendedObjects = append(previousObjects, newObjects, filterByField, filterByEquality)
+        rowsToInsert = toRows(appendedObjects) 
+    }
+
+    // ensuring max cells limit
+    const cellsToInsert = countCells(rowsToInsert)
+    console.log(`Total rows: ${rowsToInsert.length}, total cells: ${cellsToInsert}`)
+    if(cellsToInsert > MAX_CELLS) throw new Error (`You reached the max limit of ${MAX_CELLS} cells. Try inserting less rows.`)
+
+    // trimming cells
+    console.log('deleting unused cells')
+    // trimming y axis
+    await sheets.spreadsheets.batchUpdate({
+        spreadsheetId,
+        resource: trimSheetRequest(rowsToInsert.length, null, firstSheetId)
+    }).catch(e => console.log('Deleting unused rows failed, maybe there were no usuned, Error:',e.message))
+
+    // trimming x axis
+    await sheets.spreadsheets.batchUpdate({
+        spreadsheetId,
+        resource: trimSheetRequest(null, rowsToInsert[0].length, firstSheetId)
+    }).catch(e => console.log('Deleting unused columns failed, maybe there were no usuned, Error:',e.message))
+
+    // clearing cells
+    console.log('clearing cells')
+    await sheets.spreadsheets.values.clear({
+        spreadsheetId,
+        range
+    })
+
+    console.log('inserting new cells')
+    await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range,
+        valueInputOption: 'RAW',
+        resource:{values: rowsToInsert},
+    })
+    console.log('Items inserted...')
+    console.log('Finishing actor...')
+})
+
---
+++ utils.js
@@ -0,0 +1,94 @@
+const sortObj = require('sort-object')
+const md5 = require('md5')
+
+exports.countCells = (rows) => rows[0].length * rows.length
+
+exports.toObjects = (rows) => {
+    const keys = rows[0]
+    return rows.slice(1).map(row => {
+        let obj = {}
+        keys.forEach((key, i ) => obj[key] = row[i])
+        return obj
+    })
+}
+
+exports.toRows = (objects) => {
+    const header = Object.keys(objects[0])
+    const values = objects.map(object => Object.values(object))
+    return [header, ...values]
+}
+
+// works only if all objects in one array have the same keys
+exports.append = (oldObjects, newObjects, field, equality) => {
+    const oldKeys = Object.keys(oldObjects[0])
+    const newKeys = Object.keys(newObjects[0])
+    let keys = union(oldKeys, newKeys)
+    // if no field or equality - this is simple concat
+    const concated = oldObjects.concat(makeUniqueRows('append', oldObjects, newObjects, field, equality))
+    const updatedObjects = concated.map(objects => {
+        let updatedObj = objects
+        keys.forEach(key=>{
+            if(!updatedObj[key]) updatedObj[key] = null
+        })
+        return sortObj(updatedObj)
+    })
+    return updatedObjects
+}
+
+exports.trimSheetRequest = (height, width, firstSheetId) => {
+    let payload = {
+        requests: []
+    }
+    if(height){
+        payload.requests.push({
+            deleteDimension:{
+                range: {
+                    sheetId:firstSheetId,
+                    dimension: 'ROWS',
+                    startIndex: height
+                }
+            }
+        })
+    }
+    if(width){
+        payload.requests.push({
+            deleteDimension:{
+                range: {
+                    sheetId:firstSheetId,
+                    dimension: 'COLUMNS',
+                    startIndex: width
+                }
+            }
+        })
+    }
+    return payload
+}
+
+function union(setA, setB) {
+    var _union = new Set(setA);
+    for (const elem of setB) {
+        _union.add(elem);
+    }
+    return Array.from(_union);
+}
+
+function makeUniqueRows (mode, oldObjects, newObjects, field, equality){
+    const countHash = (row) => md5(Object.values(row).join(''))
+    const rowIntoKey = (row) => {
+        if(field) return row[field]
+        else if (equality) return countHash(row)
+        else throw ('Nor field or equality was provided to filterUniqueRows function')
+    }
+    if(!field && !equality) {
+        if (mode === 'append') return newObjects
+    }
+
+    let tempObj = {}
+    newObjects.forEach(row => tempObj[rowIntoKey(row)] = row)
+    oldObjects.forEach(row => tempObj[rowIntoKey(row)] = null)
+    const filteredRows = Object.values(tempObj).filter(row => !!row)
+    return filteredRows
+}
+
+// export to test
+exports.makeUniqueRows = makeUniqueRows
